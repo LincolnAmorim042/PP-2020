7c446a6a83b89bda49d4d9c546644869896b24e3
Lincoln Amorim <54185019+lolincoln@users.noreply.github.com>
2021-02-01 15:58:31 -0400

    Add files via upload

---
+++ pp/P1/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdlib.h>
+#include "numInt.h"
+
+using namespace std;
+
+int main(){
+    class numInt lista;
+    lista[5];
+    lista.ins(1);
+    lista.ins(2);
+    lista.ins(3);
+    lista.ins(4);
+    lista.ins(5);
+    lista.ins(6);
+    lista.rem();
+    lista.rem();
+    lista.rem();
+    lista.rem();
+    lista.rem();
+    lista.rem();
+    return 0;
+}
